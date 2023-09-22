a1887ecadd95e66d84686d364406cc894b322cce
Henry0802 <145728498+Henry0802@users.noreply.github.com>
2023-09-21 23:55:24 -0400

    Add files via upload

---
+++ Tareas/Ejemplos/Ejemplo 2.3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+/* El conjunto de instrucciones muestra la sintaxis de la estructura
+➥if-else en C. */
+. . .
+if (<condición>)
+<operación1>;
+else
+<operación2>;
+
+}
